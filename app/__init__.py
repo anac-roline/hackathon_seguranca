b36b219d853ce597aa2b7f4e6178326544912c94
GuilhermeBranddao <guilhermebranddao@gmail.com>
2025-06-21 02:21:17 -0300

    ok

--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 import os
-# from app.commands import create_admin
 
-
-# Inicializa a extensão
 db = SQLAlchemy()
+
 
 # Cria e configura o app
 app = Flask(__name__)
 app.config.from_object(Config)
+
+app.secret_key = 'alguma_coisa_bem_secreta'
 
 # Inicializa o app com a extensão
 db.init_app(app)
@@ -28,5 +28,8 @@ app.static_folder = os.path.join(os.path.dirname(__file__), 'static')
 # Cria o banco de dados e as tabelas se não existirem
 with app.app_context():
     db.create_all()
+
+
+
 
     # create_admin(db)
